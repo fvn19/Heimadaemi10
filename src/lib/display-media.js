9249bee7ba7e31d26bb7455b252af25ecac5921c
Flora Vuong Nu Dong <floravuong@Floras-MacBook-Pro-2.local>
2019-11-22 00:24:22 +0800

    olokid

--- src/lib/display-media.js
+++ src/lib/display-media.js
@@ -1,11 +1,10 @@
 // todo vísa í rétta hluti með import
 const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=JHjpxVKshVKB6gI30gpWXMWaBVXJmBP5Ooqoy0UH&date=';
 // breytur til þess að halda utan um html element nodes
-const title = document.getElementsByClassName('apod__title'); // titill fyrir mynd á forsíðu
-const text = document.getElementsByClassName('apod__text'); // texti fyrir mynd á forsíðu
-const img = document.getElementsByClassName('apod__image'); // mynd á forsíðu
-
-let image; // object sem inniheldur núverandi mynd á forsíðu.
+let title; // titill fyrir mynd á forsíðu
+let text; // texti fyrir mynd á forsíðu
+let img; // mynd á forsíðu
+let video; // myndband 
 
 function randomDate(start, end) {
     var d = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())),
@@ -25,7 +24,8 @@ function randomDate(start, end) {
  * ásamt titli og texta.
  */
 function getNewImage() {
-    const formatDate = randomDate(new Date('1995-06-16'), new Date());
+    //*const formatDate = randomDate(new Date('1995-06-16'), new Date());
+    const formatDate = new Date('2019-11-18');
     fetch(`${API_URL}${formatDate}`)
         .then((response) => {
             if (!response.ok) {
@@ -34,10 +34,18 @@ function getNewImage() {
             return response.json();
         })
         .then((data) => {
-            image = data;
-            console.log(image);
-            text.innerHTML = image.explanation;
-            title.innerHTML = image.title;
+            title = document.getElementsByClassName('apod__title')[0]; // titill fyrir mynd á forsíðu
+            text = document.getElementsByClassName('apod__text')[0]; // texti fyrir mynd á forsíðu
+            img = document.getElementsByClassName('apod__image')[0];
+            video = document.getElementsByClassName('apod__video')[0];
+            text.innerHTML = data.explanation;
+            title.innerHTML = data.title;
+            if (data.media_type === 'video') {
+                img.src = '';
+                video.src = data.url;
+            } else {
+                img.src = data.url;
+            }
         })
         .catch(function (error) {
             console.log(error);
